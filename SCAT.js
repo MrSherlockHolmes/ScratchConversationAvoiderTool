193b830157665e111980f8a6cb1fb25bb968c7e1
Sabri Meeran <MrSherlockHolmes123@gmail.com>
2016-04-14 14:56:19 +0100

    CreatedLol

---
+++ SCAT.js
@@ -0,0 +1,10 @@
+
+
+specially made for MegaApuTurkUltra, but anyone can use it.
+
+*/
+
+var complementSortOf = ["great", "cool", "nice", "impressive"];
+var finishOff = ["very good", "very happy", "great"];
+
+console.log(complementSortOf[Math.floor(Math.random() * complementSortOf.length)].concat(", ", finishOff[Math.floor(Math.random() * finishOff.length)]), " for you.");
